44342145e29428a43447fdaa5f6dd048917c507e
Kunal Dhingra <kdhingra55@gmail.com>
2020-06-06 15:11:45 -0400

    Initial Commit

---
+++ Artable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artable
+//
+//  Created by Kunal Dhingra on 2020-06-06.
+//  Copyright © 2020 Kunal Dhingra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
